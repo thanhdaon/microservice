0850ba360de9a3db8684220810d535ce4f0e40b6
thanhdn <daongocthanh98hy@gmail.com>
2020-03-18 18:13:34 +0700

    update

---
+++ nodejs-crawler/src/crawl_facebook.js
@@ -0,0 +1,65 @@
+const fs = require("fs");
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+
+async function run() {
+  const browser = await setupBrowser();
+  const page = await browser.newPage();
+  await loadFBCookie(page);
+  await page.goto("https://facebook.com/");
+
+  // facebook logined
+  await page.screenshot({ path: "static/fb.png" });
+  await browser.close();
+}
+
+async function setupBrowser() {
+  puppeteer.use(StealthPlugin());
+
+  const options = {
+    args: [
+      "--user-agent=`Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36`"
+    ],
+    userDataDir: "static/user_data"
+  };
+
+  return await puppeteer.launch(options);
+}
+
+async function loadFBCookie(page) {
+  const urls = [
+    "https://www.facebook.com",
+    "https://web.facebook.com",
+    "https://m.facebook.com",
+    "https://mbasic.facebook.com",
+    "https://developers.facebook.com",
+    "https://upload.facebook.com",
+    "https://mobile.facebook.com",
+    "https://business.facebook.com"
+  ];
+  const cookieText = await readTextFile("static/cookie.txt");
+  const ca = cookieText.split(";");
+  const promises = ca.map(item => {
+    if (item) {
+      const name = item.split("=")[0].trim();
+      const value = item.split("=")[1].trim();
+      const cookies = urls.map(url => ({ url, name, value }));
+      return page.setCookie(...cookies);
+    }
+  });
+
+  await Promise.all(promises);
+}
+
+function readTextFile(pathToFile) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(pathToFile, "utf8", (error, contents) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(contents);
+    });
+  });
+}
+
+run().catch(console.log);
