db1c034ce0b27b7f42b71ba99d86e935863c9921
thanhdn <daongocthanh98hy@gmail.com>
2020-03-19 00:31:17 +0700

    update

--- nodejs-crawler/src/crawl_facebook.js
+++ nodejs-crawler/src/crawl_facebook.js
@@ -6,10 +6,12 @@ async function run() {
   const browser = await setupBrowser();
   const page = await browser.newPage();
   await loadFBCookie(page);
-  await page.goto("https://facebook.com/");
 
   // facebook logined
+  await page.goto("https://mbasic.facebook.com/groups/1902499916677004");
   await page.screenshot({ path: "static/fb.png" });
+  const html = await page.evaluate(() => document.body.innerHTML);
+  console.log(html);
   await browser.close();
 }
 
@@ -19,8 +21,7 @@ async function setupBrowser() {
   const options = {
     args: [
       "--user-agent=`Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36`"
-    ],
-    userDataDir: "static/user_data"
+    ]
   };
 
   return await puppeteer.launch(options);
