d5b0cf50b9dcc0fab05fc7d64787d62184da2546
thanhdn <daongocthanh98hy@gmail.com>
2020-04-03 15:17:15 +0700

    update

--- tools/cmd/main.go
+++ tools/cmd/main.go
@@ -11,11 +11,19 @@ import (
 var (
 	PROD_RABBIT    = "amqp://ltservices:jwtm9gby6Wn9VmvR@54.38.40.255:5672"
 	STAGING_RABBIT = "amqp://admin:Mektoube2020@51.178.63.192:30002"
+	LOCAL_STRING   = "amqp://guest:guest@localhost:5672"
 )
 
 func main() {
 
-	r := rabbit.SetupRabbit(STAGING_RABBIT, []string{})
+	r := rabbit.SetupRabbit(STAGING_RABBIT, []string{
+		"moderation-final",
+		"moderation-upload-photo",
+		"moderation-image-queue",
+		"moderation-manager-image-queue",
+		"moderation-copy-done",
+		"moderation-image-waiting-for-crop-queue",
+	})
 
 	csvfile, err := os.Open("upload-photo.csv")
 	failOnError(err, "can mot open file")
