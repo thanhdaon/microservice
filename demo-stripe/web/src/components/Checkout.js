82d439ab610508cb8cf174739d6e36e99bfb0bab
thanhdn <daongocthanh98hy@gmail.com>
2020-05-08 20:21:15 +0700

    update

---
+++ demo-stripe/web/src/components/Checkout.js
@@ -0,0 +1,2 @@
+import React, { useEffect, useReducer } from "react";
+import { loadStripe } from "@stripe/stripe-js";
--- demo-stripe/web/src/index.js
+++ demo-stripe/web/src/index.js
@@ -1,10 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "App";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+import Checkout from "components/Checkout";
+
+import "css/normalize.css";
+import "css/global.css";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/success.html">
+          <div />
+        </Route>
+        <Route path="/canceled.html">
+          <div></div>
+        </Route>
+        <Route path="/">
+          <Checkout />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
