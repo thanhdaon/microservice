d6577df1bbb3d2eeba491f63788e7d9fca516b0a
thanhdn <daongocthanh98hy@gmail.com>
2020-10-31 13:46:24 +0700

    update

--- next-demo/src/tailwind.config.js
+++ next-demo/src/tailwind.config.js
@@ -1,11 +1,9 @@
 module.exports = {
   purge: {
-    mode: "all",
     content: [
       "./components/**/*.{js,ts,jsx,tsx}",
       "./pages/**/*.{js,ts,jsx,tsx}",
       "./layout/**/*.{js,ts,jsx,tsx}",
-      "./node_modules/antd/**/*.{js,ts,jsx,tsx,css,less}",
     ],
   },
   future: {
